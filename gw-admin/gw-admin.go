a444ffee7bf301a9bb3fffd72821675a868ccad4
Unknown <joe2010xtmf@163.com>
2013-08-23 00:46:49 +0800

    Done deamon gw-server

--- gw-admin/gw-admin.go
+++ gw-admin/gw-admin.go
@@ -16,14 +16,17 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"os"
+	"os/exec"
 	"runtime"
 
 	"github.com/astaxie/beego"
 )
 
 const (
-	APP_VER = "0.0.1.0819"
+	APP_VER = "0.0.1.0822"
 )
 
 func init() {
@@ -34,7 +37,7 @@ func init() {
 
 	if beego.AppConfig.String("runmode") == "pro" {
 		beego.SetLevel(beego.LevelInfo)
-
+		beego.Info("Product mode enabled")
 		beego.Info("Go Walker Admin", APP_VER)
 
 		os.Mkdir("../log", os.ModePerm)
@@ -56,5 +59,44 @@ func main() {
 
 	// For all unknown pages.
 
+	// Deamon.
+	go deamon()
+
 	beego.Run()
+}
+
+func deamon() {
+	beego.Info("Start deamon process")
+	for {
+		runServer()
+	}
+}
+
+func runServer() {
+	buf := new(bytes.Buffer)
+
+	gwsPath := "../gw-server/gw-server"
+	if runtime.GOOS == "windows" {
+		gwsPath += ".exe"
+	}
+	gws := exec.Command(gwsPath)
+	stdout, err := gws.StdoutPipe()
+	if err != nil {
+		beego.Error(err)
+	}
+
+	stderr, err := gws.StderrPipe()
+	if err != nil {
+		beego.Error(err)
+	}
+
+	err = gws.Start()
+	if err != nil {
+		beego.Error(err)
+	}
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(buf, stderr)
+	gws.Wait()
+
+	beego.Error(buf.String())
 }
